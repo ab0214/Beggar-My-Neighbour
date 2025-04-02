5a8686a839bd1f97c5b693db7f27c140b1a68474
ab0214 <89869877+ab0214@users.noreply.github.com>
2025-04-02 19:00:27 +0300

    style

--- main2.cpp
+++ main2.cpp
@@ -1,269 +1,224 @@
 #include <algorithm>
 #include <chrono>
 #include <ctime>
+#include <future>
 #include <iostream>
+#include <random>
 #include <tuple>
 #include <vector>
-#include <mutex>
-#include <thread>
-#include <future>
-#include <random>
 
-class Card
-{
+class Card {
 private:
-    int value;
+  int m_value;
 
 public:
-    Card *next = nullptr;
+  Card *m_next = nullptr;
 
-    Card(int v = 0) : value(v)
-    {
-        if (v < 0 || v > 4)
-        {
-            throw std::invalid_argument("Card's value must be between 0 and 4");
-        }
+  Card(int v = 0) : m_value(v) {
+    if (v < 0 || v > 4) {
+      throw std::invalid_argument("Card's value must be between 0 and 4");
     }
+  }
 
-    ~Card()
-    {
-        auto top = next;
-        while (top != nullptr)
-        {
-            auto current = top;
-            top = current->next;
-            current->next = nullptr; // Prevent recursion.
-            delete current;
-        }
+  ~Card() {
+    auto top = m_next;
+    while (top != nullptr) {
+      auto current = top;
+      top = current->m_next;
+      current->m_next = nullptr; // Prevent recursion.
+      delete current;
     }
+  }
 
-    int getValue() const
-    {
-        return value;
-    }
+  int getValue() const { return m_value; }
 
-    char getSymbol() const
-    {
-        static const char symbols[] = {'-', 'J', 'Q', 'K', 'A'};
-        return (value >= 1 && value <= 4) ? symbols[value] : '-';
-    }
+  char getSymbol() const {
+    static const char symbols[] = {'-', 'J', 'Q', 'K', 'A'};
+    return (m_value >= 1 && m_value <= 4) ? symbols[m_value] : '-';
+  }
 };
 
-class Cards
-{
+class Cards {
 private:
-    Card *top = nullptr;
-    Card *bottom = nullptr;
+  Card *m_top = nullptr;
+  Card *m_bottom = nullptr;
 
 public:
-    Cards() {}
+  Cards() {}
 
-    ~Cards()
-    {
-        delete top;
+  ~Cards() { delete m_top; }
+
+  Card *getBottom() { return m_bottom; }
+
+  bool isEmpty() const { return (m_top == nullptr || m_bottom == nullptr); }
+
+  void insertTop(Card *t_newCard) {
+    if (isEmpty()) {
+      m_top = t_newCard;
+      m_bottom = t_newCard;
+      t_newCard->m_next = nullptr;
+      return;
     }
 
-    Card *getBottom() { return bottom; }
+    t_newCard->m_next = m_top;
+    m_top = t_newCard;
+  }
 
-    bool isEmpty() const
-    {
-        return (top == nullptr || bottom == nullptr);
+  void insertBottom(Card *t_newCard) {
+    if (isEmpty()) {
+      m_top = t_newCard;
+      m_bottom = t_newCard;
+      t_newCard->m_next = nullptr;
+      return;
     }
 
-    void insertTop(Card *newCard)
-    {
-        if (isEmpty())
-        {
-            top = newCard;
-            bottom = newCard;
-            newCard->next = nullptr;
-            return;
-        }
+    m_bottom->m_next = t_newCard;
+    m_bottom = t_newCard;
+    m_bottom->m_next = nullptr;
+  }
 
-        newCard->next = top;
-        top = newCard;
+  void splice(Cards *t_newCards) {
+    if (t_newCards->isEmpty()) {
+      return;
     }
 
-    void insertBottom(Card *newCard)
-    {
-        if (isEmpty())
-        {
-            top = newCard;
-            bottom = newCard;
-            newCard->next = nullptr;
-            return;
-        }
-
-        bottom->next = newCard;
-        bottom = newCard;
-        bottom->next = nullptr;
+    if (isEmpty()) {
+      m_top = t_newCards->m_top;
+      m_bottom = t_newCards->m_bottom;
+      return;
     }
 
-    void splice(Cards *newCards)
-    {
-        if (newCards->isEmpty())
-        {
-            return;
-        }
+    m_bottom->m_next = t_newCards->m_top;
+    m_bottom = t_newCards->m_bottom;
 
-        if (isEmpty())
-        {
-            top = newCards->top;
-            bottom = newCards->bottom;
-            return;
-        }
+    t_newCards->clear();
+  }
 
-        bottom->next = newCards->top;
-        bottom = newCards->bottom;
+  void clear() {
+    m_top = nullptr;
+    m_bottom = nullptr;
+  }
 
-        newCards->clear();
+  Card *pop() {
+    if (m_top == nullptr)
+      return nullptr;
+    auto temp = m_top;
+    m_top = m_top->m_next;
+    if (m_top == nullptr)
+      m_bottom = nullptr;
+    return temp;
+  }
+
+  friend std::ostream &operator<<(std::ostream &t_ostream,
+                                  const Cards &t_cards) {
+    Card *current = t_cards.m_top;
+    while (current != nullptr) {
+      t_ostream << current->getSymbol();
+      current = current->m_next;
     }
-
-    void clear()
-    {
-        top = nullptr;
-        bottom = nullptr;
-    }
-
-    Card *pop()
-    {
-        if (top == nullptr)
-            return nullptr;
-        auto temp = top;
-        top = top->next;
-        if (top == nullptr)
-            bottom = nullptr;
-        return temp;
-    }
-
-    friend std::ostream &operator<<(std::ostream &os, const Cards &cards)
-    {
-        Card *current = cards.top;
-        while (current != nullptr)
-        {
-            os << current->getSymbol();
-            current = current->next;
-        }
-        return os;
-    }
+    return t_ostream;
+  }
 };
 
-std::tuple<int, int> play_game()
-{
+std::tuple<int, int> playGame() {
+  std::vector<int> deck(52, 0); // 52 cards that will be dealt to the players
+  for (int i = 0; i < 16; ++i)  // Add four of each picture card to the deck
+                                // (replacing number cards)
+    deck[i] = i / 4 + 1;
 
-    std::vector<int> deck(52, 0); // 52 cards that will be dealt to the players
-    for (int i = 0; i < 16; ++i)  // Add four of each picture card to the deck (replacing number cards)
-        deck[i] = i / 4 + 1;
+  std::random_device rd; // Random device to get a seed
+  std::mt19937 g(rd());  // Mersenne Twister engine initialized with the seed
+  std::shuffle(deck.begin(), deck.end(), g); // Shuffle the deck
 
-    std::random_device rd;                     // Random device to get a seed
-    std::mt19937 g(rd());                      // Mersenne Twister engine initialized with the seed
-    std::shuffle(deck.begin(), deck.end(), g); // Shuffle the deck
+  Cards players[] = {Cards(), Cards()};
+  for (int i = 0; i < deck.size(); i += 2) {
+    players[0].insertBottom(new Card(deck[i]));
+    players[1].insertBottom(new Card(deck[i + 1]));
+  }
 
-    Cards players[] = {Cards(), Cards()};
-    for (int i = 0; i < deck.size(); i += 2)
-    {
-        players[0].insertBottom(new Card(deck[i]));
-        players[1].insertBottom(new Card(deck[i + 1]));
-    }
+  Cards pile = Cards();
 
-    Cards pile = Cards();
+  int cards = 0;
+  int tricks = 0;
 
-    int cards = 0;
-    int tricks = 0;
+  int turn = 0;
+  int penalty = 0;
 
-    int turn = 0;
-    int penalty = 0;
+  while (!players[0].isEmpty() && !players[1].isEmpty()) {
+    ++cards;
 
-    while (!players[0].isEmpty() && !players[1].isEmpty())
-    {
-        // std::cout << "Cards    : " << cards << std::endl
-        //           << "Tricks   : " << tricks << std::endl
-        //           << "Player 1 : " << players[0] << std::endl
-        //           << "Player 2 : " << players[1] << std::endl
-        //           << "Pile     : " << pile << std::endl
-        //           << std::endl;
+    if (penalty > 0) {
+      pile.insertBottom(players[turn].pop());
 
-        ++cards;
-
-        if (penalty > 0)
-        {
-            pile.insertBottom(players[turn].pop());
-
-            if (pile.getBottom()->getValue() > 0)
-            {
-                turn = !turn;
-                penalty = pile.getBottom()->getValue();
-                tricks++;
-                continue;
-            }
-
-            else if (--penalty == 0)
-            {
-                turn = !turn;
-                players[turn].splice(&pile);
-            }
-
-            continue;
-        }
-
-        pile.insertBottom(players[turn].pop());
-        penalty = pile.getBottom()->getValue();
+      if (pile.getBottom()->getValue() > 0) {
         turn = !turn;
+        penalty = pile.getBottom()->getValue();
+        tricks++;
+        continue;
+      }
+
+      else if (--penalty == 0) {
+        turn = !turn;
+        players[turn].splice(&pile);
+      }
+
+      continue;
     }
 
-    return {cards, tricks};
+    pile.insertBottom(players[turn].pop());
+    penalty = pile.getBottom()->getValue();
+    turn = !turn;
+  }
+
+  return {cards, tricks};
 }
 
-std::tuple<int, int> play_games(int games)
-{
-    int max_cards = 0;
-    int max_tricks = 0;
+std::tuple<int, int> playGames(int t_games) {
+  int max_cards = 0;
+  int max_tricks = 0;
 
-    for (int i = 0; i < games; ++i)
-    {
-        auto [cards, tricks] = play_game();
-        max_cards = cards > max_cards ? cards : max_cards;
-        max_tricks = tricks > max_tricks ? tricks : max_tricks;
-    }
+  for (int i = 0; i < t_games; ++i) {
+    auto [cards, tricks] = playGame();
+    max_cards = cards > max_cards ? cards : max_cards;
+    max_tricks = tricks > max_tricks ? tricks : max_tricks;
+  }
 
-    return {max_cards, max_tricks};
+  return {max_cards, max_tricks};
 }
 
-int main()
-{
-    int num_threads = 6;
-    int games_per_thread = 100000;
-    int max_cards = 0;
-    int max_tricks = 0;
+int main() {
+  int num_threads = 6;
+  int games_per_thread = 100000;
+  int max_cards = 0;
+  int max_tricks = 0;
 
-    while (true)
-    {
-        auto start = std::chrono::high_resolution_clock::now();
+  while (true) {
+    auto start = std::chrono::high_resolution_clock::now();
 
-        std::vector<std::future<std::tuple<int, int>>> futures;
+    std::vector<std::future<std::tuple<int, int>>> futures;
 
-        for (int i = 0; i < num_threads; ++i)
-        {
-            futures.push_back(std::async(std::launch::async, play_games, games_per_thread));
-        }
-
-        for (auto &f : futures)
-        {
-            auto [cards, tricks] = f.get();
-            max_cards = cards > max_cards ? cards : max_cards;
-            max_tricks = tricks > max_tricks ? tricks : max_tricks;
-        }
-
-        auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> elapsed_s = end - start;
-        std::chrono::duration<double, std::micro> elapsed_us = end - start;
-
-        int games = num_threads * games_per_thread;
-        int avg_us = elapsed_us.count() / games;
-        std::cout << "Played " << games << " games in " << elapsed_s.count() << " seconds (Avg: " << avg_us << " us per game)." << std::endl
-                  << "Most cards so far: " << max_cards << std::endl
-                  << "Most tricks so far: " << max_tricks << std::endl;
+    for (int i = 0; i < num_threads; ++i) {
+      futures.push_back(
+          std::async(std::launch::async, playGames, games_per_thread));
     }
 
-    return 0;
+    for (auto &f : futures) {
+      auto [cards, tricks] = f.get();
+      max_cards = cards > max_cards ? cards : max_cards;
+      max_tricks = tricks > max_tricks ? tricks : max_tricks;
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed_s = end - start;
+    std::chrono::duration<double, std::micro> elapsed_us = end - start;
+
+    int games = num_threads * games_per_thread;
+    int avg_us = elapsed_us.count() / games;
+    std::cout << "Played " << games << " games in " << elapsed_s.count()
+              << " seconds (Avg: " << avg_us << " us per game)." << std::endl
+              << "Most cards so far: " << max_cards << std::endl
+              << "Most tricks so far: " << max_tricks << std::endl;
+  }
+
+  return 0;
 }
